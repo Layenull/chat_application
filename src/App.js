1ef2d0e08f6e3a277a57608706a884b9733895db
layenull <ayomidela14@gmail.com>
2022-10-28 14:17:02 +0100

    added my own pass key

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = 'd78c9600-0440-44e9-b734-5708b858eca1';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
